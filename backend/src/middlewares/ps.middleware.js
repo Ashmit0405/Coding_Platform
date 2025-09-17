127df40280d42b7974704ace1b18c2610b5be8e9
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2025-09-17 14:50:09 +0530

    Added to check the problem setter role

---
+++ backend/src/middlewares/ps.middleware.js
@@ -0,0 +1,13 @@
+import { ApiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asynchandler";
+
+const check_ps=asyncHandler(async (req,res,next)=>{
+    const user=req.user;
+    if(!user) throw new ApiError(400,"User Not Found");
+    if(user.role!=="problem_setter"&&user.role!=="admin"){
+        throw new ApiError(400,"This role is not allowed"); 
+    }
+    next();
+})
+
+export {check_ps};
